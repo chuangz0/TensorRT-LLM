d09ac957da20ec7152550bc93a25e6df40621a42
Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
2026-04-15 08:06:37 +0000

    use multi thread for kv transfer and binding request_id+peer_rank in one thread
    Signed-off-by: Chuang Zhu <111838961+chuangz0@users.noreply.github.com>

--- tensorrt_llm/_torch/disaggregation/native/transfer.py
+++ tensorrt_llm/_torch/disaggregation/native/transfer.py
@@ -159,6 +159,7 @@ class SendTaskBase:
         self.status = TaskStatus.INIT
         self._event = threading.Event()
         self._exception: Optional[Exception] = None
+        self.lock = threading.Lock()
         self._params = params
         self._unique_rid: Optional[int] = params.disagg_request_id
         self._perf_timer = PerfTimer() if perf_log_manager.enabled else None
@@ -350,9 +351,10 @@ class Sender(SenderBase):
         return session
 
     def _enqueue(self, write_meta: WriteMeta):
-        # Distribute tasks to threads by unique_rid to ensure same session's tasks
-        # are processed by the same thread in order
-        thread_idx = write_meta.unique_rid % self._num_threads
+        # Route by (unique_rid, peer_rank) so that:
+        # - Same peer's slices stay ordered on one thread (is_last_slice correctness)
+        # - Different peers can run on different threads (better load balancing)
+        thread_idx = hash((write_meta.unique_rid, write_meta.peer_rank)) % self._num_threads
         self._send_task_queues[thread_idx].put(write_meta)
 
     def _process_task_queue(self, thread_idx: int):
@@ -495,16 +497,20 @@ class Sender(SenderBase):
             ]
         )
 
-        task.transferred_count += 1
         if timer:
             timer.record_task_end(write_meta.peer_rank)
         ri = self._registrar.self_rank_info
         task.print_perf_info(write_meta.peer_rank, ri.instance_name, ri.instance_rank)
-        if task.transferred_count > write_meta.expected_transfers:
+
+        with task.lock:
+            task.transferred_count += 1
+            count = task.transferred_count
+
+        if count > write_meta.expected_transfers:
             session.set_exception(
                 f"KV slice {write_meta.slice_id} received more than {write_meta.expected_transfers} transfers"
             )
-        elif task.transferred_count == write_meta.expected_transfers:
+        elif count == write_meta.expected_transfers:
             if task.is_done:
                 task.status = TaskStatus.ERROR
                 session.set_exception(
@@ -552,18 +558,22 @@ class Sender(SenderBase):
             ]
         )
 
-        aux_task._transfer_count += 1
         if timer:
             timer.record_task_end(write_meta.peer_rank)
         ri = self._registrar.self_rank_info
         aux_task.print_perf_info(write_meta.peer_rank, ri.instance_name, ri.instance_rank)
-        if aux_task._transfer_count == write_meta.expected_transfers:
+
+        with aux_task.lock:
+            aux_task._transfer_count += 1
+            count = aux_task._transfer_count
+
+        if count == write_meta.expected_transfers:
             if aux_task.is_done:
                 aux_task.status = TaskStatus.ERROR
                 session.set_exception("aux task already resolved on completion")
             else:
                 aux_task.complete()
-        elif aux_task._transfer_count > write_meta.expected_transfers:
+        elif count > write_meta.expected_transfers:
             session.set_exception(
                 f"aux task received more than {write_meta.expected_transfers} transfers"
             )
