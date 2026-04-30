d956bc8421d71d9773e8c438e5a7c4c9075c57de
Yi Zhang <187001205+yizhang-nv@users.noreply.github.com>
2026-04-30 10:10:46 +0800

    [TRTLLM-12111][feat] Add V2 KV cache event support (#13589)
    Signed-off-by: Yi Zhang <187001205+yizhang-nv@users.noreply.github.com>
    Signed-off-by: Fanrong Li <lfr-0531@users.noreply.github.com>

--- tensorrt_llm/runtime/kv_cache_manager_v2/__init__.py
+++ tensorrt_llm/runtime/kv_cache_manager_v2/__init__.py
@@ -49,6 +49,17 @@ from ._core import (
     ScratchDesc,
     _KVCache,
 )
+from ._event_manager import (
+    KVCacheCreatedData,
+    KVCacheEvent,
+    KVCacheEventDiff,
+    KVCacheEventManager,
+    KVCacheRemovedData,
+    KVCacheStoredBlockData,
+    KVCacheStoredData,
+    KVCacheUpdatedData,
+    UniqueToken,
+)
 from ._life_cycle_registry import LayerGroupId, LifeCycleId
 from ._storage import BufferId
 
@@ -59,6 +70,15 @@ __all__ = [
     "TokenIdExt",
     "KVCacheManager",
     "_KVCache",
+    "KVCacheCreatedData",
+    "KVCacheEvent",
+    "KVCacheEventDiff",
+    "KVCacheEventManager",
+    "KVCacheRemovedData",
+    "KVCacheStoredBlockData",
+    "KVCacheStoredData",
+    "KVCacheUpdatedData",
+    "UniqueToken",
     "BeamIndex",
     "DEFAULT_BEAM_INDEX",
     "LayerId",
